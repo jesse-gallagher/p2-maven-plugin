5cb081f813961cefb90edbc6f42c82254ffe52ce
Jesse Gallagher <jesse@secondfoundation.org>
2023-03-26 13:44:38 -0400

    Revert "Convert Groovy classes to Java"
    This reverts commit d33e47c7292172e0a0b9335d126fcbb0206c24dd.
    
    # Conflicts:
    #	src/main/java/org/reficio/p2/resolver/eclipse/impl/DefaultEclipseResolver.java
    #	src/main/java/org/reficio/p2/resolver/eclipse/impl/FileBinaryCategory.java
    #	src/main/java/org/reficio/p2/resolver/maven/impl/facade/AetherEclipseFacade.java
    #	src/main/java/org/reficio/p2/resolver/maven/impl/facade/AetherFacade.java
    #	src/main/java/org/reficio/p2/resolver/maven/impl/facade/AetherSonatypeFacade.java

--- src/main/groovy/org/reficio/p2/resolver/eclipse/impl/FileBinaryCategory.groovy
+++ src/main/groovy/org/reficio/p2/resolver/eclipse/impl/FileBinaryCategory.groovy
@@ -16,24 +16,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.reficio.p2.resolver.eclipse.impl;
+package org.reficio.p2.resolver.eclipse.impl
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-
-import com.ibm.commons.util.io.StreamUtil;
-
-public class FileBinaryCategory {
-	public static void leftShift(File file, URL url) throws IOException {
-		try(
-			InputStream is = url.openStream();
-			OutputStream os = new FileOutputStream(file)
-		) {
-			StreamUtil.copyStream(is, os);
-		}
+class FileBinaryCategory {
+    def static leftShift(File file, URL url) {
+        url.withInputStream { is ->
+            file.withOutputStream { os ->
+                def bs = new BufferedOutputStream(os)
+                bs << is
+            }
+        }
     }
 }
+
